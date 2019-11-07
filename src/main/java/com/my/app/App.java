e88f6b37b0feb8a0e982400990e163433d92f274
paulo <paulohess@yahoo.com>
2019-11-07 15:10:02 -0800

    first commit

---
+++ src/main/java/com/my/app/App.java
@@ -0,0 +1,7 @@
+package com.my.app;
+
+public class App {
+    public static void main(String[] str){
+        System.out.println("hello world !");
+    }
+}
---
+++ src/main/java/com/my/app/Gender.java
@@ -0,0 +1,5 @@
+package com.my.app;
+
+public enum Gender {
+    MALE,FEMALE;
+}
